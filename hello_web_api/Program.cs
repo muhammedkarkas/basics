6714e8bc18eba36960fa4cf2efcdd9434224ebc1
muhammedkarkas <muhammed.karkas@gmail.com>
2023-10-23 15:41:48 +0300

    .

---
+++ hello_web_api/Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Services(container)
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); //WebApplication Create
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.Run();
