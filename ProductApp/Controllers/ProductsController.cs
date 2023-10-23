d9066d0d39bbd6b5db334cc6cb803745386db5ce
muhammedkarkas <muhammed.karkas@gmail.com>
2023-10-23 16:37:31 +0300

    .

---
+++ ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProdutcs()
+        {
+            var products = new List<Product>()
+            {
+                new Product { Id = 1, ProductName = "Computer"},
+                new Product { Id = 2, ProductName = "Keyboard"},
+                new Product { Id = 3, ProductName = "Mouse"}
+            };
+            //debug ve cmd üzerine işleme dair log kaydı eklendi.İstenirse database ortamına da log kayıtları eklenebilir.
+            _logger.LogInformation("GetAllProducts action has been called.");
+            return Ok(products);
+        }
+    }
+}
